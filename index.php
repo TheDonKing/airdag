9602f37c85cb131c38b0031d4c8a1b695c5af6fb
TheDonKing <starspersona@gmail.com>
2015-07-16 20:30:01 +0300

    Закончен раздел Promo

--- index.php
+++ index.php
@@ -1200,13 +1200,30 @@
             <img src="img/iphone.png" alt=""/>
         </div>
         <div class="promoText">
-            <div class="promoTextBoxUp"></div>
-            <div class="promoTextBoxBottom"></div>
+            <div class="promoTextBoxUp">
+                <p>Лучшие цены<br>
+                    в вашем сматфоне <br/>
+                    или планшете</p>
+            </div>
+            <div class="promoTextBoxBottom">
+                    <div class="box1">
+                        <p><i class="fa fa-apple"></i><a href="">Iphone</a></p>
+                        <p><i class="fa fa-apple"></i><a href="">Ipad</a></p>
+                        <p><i class="fa fa-android"></i><a href="">Android</a></p>
+                    </div>
+                    <div class="box2">
+                        <p><a href="">Подробнее о приложении с поиском авиабилетов и отелей</a></p>
+                    </div>
+            </div>
         </div>
     </div>
-
 </div>
-
+    <div class="country">
+        <div class="row">
+            <div class="box"></div>
+            <div class="box2"></div>
+        </div>
+    </div>
 
 </div>
 
