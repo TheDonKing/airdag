683b2aed957d75ba762c68b642023b4dfb0eef99
TheDonKing <starspersona@gmail.com>
2015-07-16 15:16:33 +0300

    Завершение блока популярные направления

--- index.php
+++ index.php
@@ -936,83 +936,254 @@
                         <div class="destinationsContentBlockBox">
                             <div class="container">
                                 <div class="view view-sixth">
-                                    <h3>Стамбул</h3>
+                                    <h3>Стамбул <span>Турция</span></h3>
                                     <img src="img/view/1.jpg" />
-
+                                        <div class="mask">
+                                            <h2>Стамбул</h2>
+                                            <a href="#" class="info">Подробнее</a>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/2.jpg" />
                                     <div class="mask">
                                         <h2>Стамбул</h2>
-                                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
-                                        <a href="#" class="info">Read More</a>
-
+                                        <a href="#" class="info">Подробнее</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                    </div>
-                    <div class="destinationsContentBlockResult">
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                    </div>
-                    <div class="destinationsContentBlockResult">
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                    </div>
-                    <div class="destinationsContentBlockResult">
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                    </div>
-                    <div class="destinationsContentBlockResult">
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
-                        </div>
-                        <div class="destinationsContentBlockBox">
-                            <div class="container"></div>
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/3.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="destinationsContentBlockBox">
                             <div class="container">
-
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/4.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="destinationsContentBlockResult">
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/5.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/6.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/7.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/1.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="destinationsContentBlockResult">
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/2.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/3.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/4.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/5.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="destinationsContentBlockResult">
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/6.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/7.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/3.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/4.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="destinationsContentBlockResult">
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/5.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/6.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/1.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="destinationsContentBlockBox">
+                            <div class="container">
+                                <div class="view view-sixth">
+                                    <h3>Стамбул <span>Турция</span></h3>
+                                    <img src="img/view/5.jpg" />
+                                    <div class="mask">
+                                        <h2>Стамбул</h2>
+                                        <a href="#" class="info">Подробнее</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
